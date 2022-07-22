78069aa55d7685b0c1397896091b61a77d2c75fd
Beedessy <56188746+beedessy@users.noreply.github.com>
2022-07-22 12:27:52 +0400

    IUser interface

---
+++ Agile Assignment/src/DIP/IUser.java
@@ -0,0 +1,15 @@
+
+interface IUser {
+
+    public void login();
+
+    public void purchaseMovie();
+
+    public void watch();
+
+    public void addFavorite();
+
+    public void rateMovie();
+
+    public void logout();
+}
